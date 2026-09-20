b56cda6a39c3983da2f13df942c4fafa589236f0
OseMine <98693947+OseMine@users.noreply.github.com>
2026-09-20 17:47:22 +0200

    refactor(calendar): simplify error handling in calendar methods

--- lib/services/calendar_platform.dart
+++ lib/services/calendar_platform.dart
@@ -1,24 +1,18 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import '../main.dart';
 
 class CalendarPlatform {
   static const MethodChannel _channel = MethodChannel('untisplus/calendar');
 
   static Future<List<Map<String, dynamic>>> getCalendars() async {
-    try {
-      final result = await _channel.invokeMethod('getCalendars');
-      final calendars = List<Map<String, dynamic>>.from(result as List);
-      debugPrint('[CalendarPlatform] getCalendars: ${calendars.length} calendars');
-      for (final cal in calendars) {
-        debugPrint('[CalendarPlatform]   - ${cal['name']} (id: ${cal['id']}, default: ${cal['isDefault']})');
-      }
-      return calendars;
-    } catch (e) {
-      debugPrint('[CalendarPlatform] getCalendars error: $e');
-      return [];
+    final result = await _channel.invokeMethod('getCalendars');
+    final calendars = List<Map<String, dynamic>>.from(result as List);
+    debugPrint('[CalendarPlatform] getCalendars: ${calendars.length} calendars');
+    for (final cal in calendars) {
+      debugPrint('[CalendarPlatform]   - ${cal['name']} (id: ${cal['id']}, default: ${cal['isDefault']})');
     }
+    return calendars;
   }
 
   static Future<String?> createCalendar({
@@ -26,18 +20,13 @@ class CalendarPlatform {
     required String color,
     String accountName = 'Untis+',
   }) async {
-    try {
-      final result = await _channel.invokeMethod('createCalendar', {
-        'name': name,
-        'color': color,
-        'accountName': accountName,
-      });
-      debugPrint('[CalendarPlatform] createCalendar: $name -> $result');
-      return result;
-    } catch (e) {
-      debugPrint('[CalendarPlatform] createCalendar error: $e');
-      return null;
-    }
+    final result = await _channel.invokeMethod('createCalendar', {
+      'name': name,
+      'color': color,
+      'accountName': accountName,
+    });
+    debugPrint('[CalendarPlatform] createCalendar: $name -> $result');
+    return result;
   }
 
   static Future<List<Map<String, dynamic>>> getEvents({
@@ -45,32 +34,24 @@ class CalendarPlatform {
     DateTime? start,
     DateTime? end,
   }) async {
-    try {
-      final startMs = start?.millisecondsSinceEpoch ?? 0;
-      final endMs = end?.millisecondsSinceEpoch ?? 0;
-      final result = await _channel.invokeMethod('getEvents', {
-        'calendarId': calendarId,
-        'startMs': startMs,
-        'endMs': endMs,
-      });
-      return List<Map<String, dynamic>>.from(result as List);
-    } catch (e) {
-      return [];
-    }
+    final startMs = start?.millisecondsSinceEpoch ?? 0;
+    final endMs = end?.millisecondsSinceEpoch ?? 0;
+    final result = await _channel.invokeMethod('getEvents', {
+      'calendarId': calendarId,
+      'startMs': startMs,
+      'endMs': endMs,
+    });
+    return List<Map<String, dynamic>>.from(result as List);
   }
 
   static Future<bool> deleteEvent({
     required String calendarId,
     required String eventId,
   }) async {
-    try {
-      return await _channel.invokeMethod('deleteEvent', {
-        'calendarId': calendarId,
-        'eventId': eventId,
-      }) as bool? ?? false;
-    } catch (e) {
-      return false;
-    }
+    return await _channel.invokeMethod('deleteEvent', {
+      'calendarId': calendarId,
+      'eventId': eventId,
+    }) as bool? ?? false;
   }
 
   static Future<bool> addEvent({
@@ -83,23 +64,18 @@ class CalendarPlatform {
     List<int>? reminderMinutes,
     bool allDay = false,
   }) async {
-    try {
-      debugPrint('[CalendarPlatform] addEvent: title=$title, calendarId=$calendarId');
-      final result = await _channel.invokeMethod('addEvent', {
-        'title': title,
-        'description': description,
-        'startMs': startMs,
-        'endMs': endMs,
-        'location': location,
-        'calendarId': calendarId,
-        'reminderMinutes': reminderMinutes ?? [15, 60],
-        'allDay': allDay,
-      });
-      debugPrint('[CalendarPlatform] addEvent result: $result');
-      return result as bool? ?? false;
-    } catch (e) {
-      debugPrint('[CalendarPlatform] addEvent error: $e');
-      return false;
-    }
+    debugPrint('[CalendarPlatform] addEvent: title=$title, calendarId=$calendarId');
+    final result = await _channel.invokeMethod('addEvent', {
+      'title': title,
+      'description': description,
+      'startMs': startMs,
+      'endMs': endMs,
+      'location': location,
+      'calendarId': calendarId,
+      'reminderMinutes': reminderMinutes ?? [15, 60],
+      'allDay': allDay,
+    });
+    debugPrint('[CalendarPlatform] addEvent result: $result');
+    return result as bool? ?? false;
   }
 }
